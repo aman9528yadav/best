4e4ff7ac86f8283215a0278a58c238e3561ae6ab
Aman Yadav <amanyadavyadav9458@gmail.com>
2025-09-30 06:39:54 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/dev/manage-updates/page.tsx
+++ src/app/dev/manage-updates/page.tsx
@@ -35,6 +35,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -331,6 +332,8 @@ export default function ManageUpdatesPage() {
     </div>
   );
 }
+    
+
     
 
     
